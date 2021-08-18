de8b9742b8a3288832036cf002261c7a906adfe0
danahinds <danahinds@gmail.com>
2021-08-18 18:51:07 -0400

    bug fixed rewrite a const for define property

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 'use strict';
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const dice = document.querySelector('.dice');
+const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 score0El.textContent = '0';
 score1El.textContent = '0';
-dice.classList.add('hidden');
+diceEl.classList.add('hidden');
 
 //Rolling dice functionality/
 const scores = [0, 0];
 let currentScore = 0;
-let activePlayer = 0;
+let activePlayer = 0; //activePlayer starts with 0 in the array
+
 btnRoll.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
-  dice.classList.remove('hidden');
-  dice.src = `dice-${dice}.png`;
+  //Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
   if (dice !== 1) {
     currentScore += dice;
-    document.getElementById('current--${activePlayer}').textContent =
+    document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
-    current0EL.textContent = currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
   }
 });
